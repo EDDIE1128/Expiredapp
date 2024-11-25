0e7c546b03fce92a5583268f7bd8e322a8e1845f
EDDIE1128 <ranb@andrew.cmu.edu>
2024-11-25 13:30:49 -0500

    add confidence

--- Expired/ViewController.swift
+++ Expired/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
 
 
+    
     @IBOutlet weak var Imageview: UIImageView!
+    @IBOutlet weak var classLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var ConfidenceLabel: UILabel!
+    
+    
     @IBAction func Picbutton(_ sender: UIButton) {
         let imagePickerController = UIImagePickerController()
             imagePickerController.delegate = self
@@ -24,11 +32,64 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 Imageview.image = image
+                classifyImage(image: image)
             }
             dismiss(animated: true, completion: nil)
         }
+    
+    func classifyImage(image: UIImage) {
+        guard let url = URL(string: "http://10.0.0.136:5000/classify") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
 
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        let boundary = UUID().uuidString
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        let imageData = image.jpegData(compressionQuality: 0.8)!
+        var body = Data()
+        body.append("--\(boundary)\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"image\"; filename=\"image.jpg\"\r\n".data(using: .utf8)!)
+        body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+        body.append(imageData)
+        body.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+
+        request.httpBody = body
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+
+            // Parse the response
+            if let result = try? JSONSerialization.jsonObject(with: data) as? [String: String],
+               let classification = result["classification"],
+               let confidence = result["confidence"],
+               let savedPath = result["saved_path"]{
+                DispatchQueue.main.async {
+                    self.showClassificationResult(classification: classification, confidence: confidence, savedPath: savedPath)
+                }
+            }
+        }
+        task.resume()
+    }
+
+//    func showClassificationResult(classification: String, savedPath: String) {
+//        let alert = UIAlertController(
+//            title: "Classification Result",
+//            message: "Class: \(classification)\nSaved at: \(savedPath)",
+//            preferredStyle: .alert
+//        )
+//        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//        self.present(alert, animated: true, completion: nil)
+//    }
+    func showClassificationResult(classification: String, confidence: String, savedPath: String) {
+        classLabel.text = "Class: \(classification)"
+        ConfidenceLabel.text = "Confidence: \(confidence)"
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             dismiss(animated: true, completion: nil)
         }
 
